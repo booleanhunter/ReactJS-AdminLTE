8d108fc7d73ea87e91a5e5286554e1e311475b1a
Adrien HARNAY <adrien-harnay@wanadoo.fr>
2016-04-19 12:28:56 +0200

    routes.js made compliant to airbnb norm

--- reactjs-adminlte/routes.js
+++ reactjs-adminlte/routes.js
@@ -1,41 +1,38 @@
-function initialize(app){
+function initialize(app) {
+	// These are the API end points that you can write.
+	// Setting up an event listener for GET request to '/'
+  app.get('/', (req, res) => {
+    console.log('request to / received');
+    res.render('dashboard.html');
+  });
 
-	//These are the API end points that you can write.
+  app.get('/starter.html', (req, res) => {
+    res.render('starter.html');
+  });
 
-	//Setting up an event listener for GET request to '/' 
-	app.get('/', function(req, res){ 
-		console.log('request to / received');
-        res.render('dashboard.html');		
-	});
+  app.get('/index.html', (req, res) => {
+    res.render('index.html');
+  });
 
-    app.get('/starter.html', function(req, res){
-        res.render('starter.html');
-    });
+  app.get('/index2.html', (req, res) => {
+    res.render('index2.html');
+  });
 
-    app.get('/index.html', function(req, res){
-        res.render('index.html');
-    });
+  app.get('/widgets.html', (req, res) => {
+    res.render('widgets.html');
+  });
 
-    app.get('/index2.html', function(req, res){
-        res.render('index2.html');
-    });
+  app.get('/buttons.html', (req, res) => {
+    res.render('buttons.html');
+  });
 
-    app.get('/widgets.html', function(req, res){
-        res.render('widgets.html');
-    });
+  app.get('/UI/general.html', (req, res) => {
+    res.render('general.html');
+  });
 
-    app.get('/buttons.html', function(req, res){
-        res.render('buttons.html');
-    });
-
-    app.get('/UI/general.html', function(req, res){
-        res.render('general.html');
-    });
-
-    app.get('/timeline.html', function(req, res){
-        res.render('timeline.html');
-    });
-
+  app.get('/timeline.html', (req, res) => {
+    res.render('timeline.html');
+  });
 }
 
 exports.initialize = initialize;
