8d3b771cf77d1ca3ca27211378cf026c2166bc69
GaldanM <g.moulinneuf@outlook.fr>
2016-04-20 11:41:37 +0200

    changes

--- public/src/login/js/main.js
+++ public/src/login/js/main.js
@@ -1,7 +1,8 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginForm from './LoginForm.js';
 
 ReactDOM.render(
   <LoginForm />,
-  document.getElementById('login-container')
+  document.getElementById('login-form')
 );
